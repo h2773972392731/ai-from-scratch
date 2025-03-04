aad9eb93c2029cb05d5c2f17e7d1505df113f4c5
“pc_at_home” <pc_at_home@qq.com>
2025-03-04 22:45:22 +0800

    unicorn

--- uvicorn_server.py
+++ uvicorn_server.py
@@ -1,7 +1,6 @@
 # 在命令行中执行以下命令：
-# uvicorn main:app --reload
+# uvicorn uvicorn_server:app --reload
 # uvicorn uvicorn_server:app --host 127.0.0.1 --port 8000
-# uvicorn uvicorn_server:app --host 127.0.0.1 --port 8000 --log-level debug
 # 这将启动一个名为 main 的 ASGI 应用程序，使用 Uvicorn 服务器运行在本地主机的默认端口 8000 上，并监听根路径 / 的 GET 请求。在浏览器中访问 http://localhost:8000，将看到 "Hello, World!" 的消息。
 
 from fastapi import FastAPI
