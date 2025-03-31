e05691ae462d16bd75e0bf96fa973103f0d9296a
“pc_at_home” <pc_at_home@qq.com>
2025-03-31 22:09:45 +0800

    pytorch-load-model-llama2-*

--- book/chinese_llama_alpaca/pytorch-load-model-llama2.py
+++ book/chinese_llama_alpaca/pytorch-load-model-llama2.py
@@ -1,10 +1,14 @@
 # Use a pipeline as a high-level helper
 from transformers import pipeline
 
-pipe = pipeline("text-generation", model="meta-llama/Llama-2-7b-hf")
+pipe = pipeline("text-generation", model="meta-llama/Llama-2-7b-hf", framework="pt")
 
 # Load model directly
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
+base_model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-hf")
 tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-hf")
-model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-hf")
+
+pipe = pipeline("text-generation", model=base_model, tokenizer=tokenizer)
+output = pipe("你好，请介绍下你自己", max_length=50)
+print(f"output:{output}")
