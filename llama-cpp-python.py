be172b4cfdc3a6111b3dabaa11be1b77f1e7efe3
“pc_at_company” <pc_at_company@qq.com>
2025-04-03 16:38:10 +0800

    用 llama_cpp_python运行模型，llama_cpp_python底层调用的是llama.cpp，llama.cpp是推理引擎，是可以加载模型的，因此llama_cpp_python可以运行模型

--- llama-cpp-python.py
+++ llama-cpp-python.py
@@ -1,7 +1,13 @@
-# 用 llama_cpp_python 运行模型
+# 用 llama_cpp_python运行模型，llama_cpp_python底层调用的是llama.cpp，llama.cpp是推理引擎，是可以加载模型的，因此llama_cpp_python可以运行模型
 
 from llama_cpp import Llama
 
-llm = Llama(model_path="C:/Users/Administrator/.cache/modelscope/hub/models/Xorbits/Qwen-7B-Chat-GGUF/Qwen-7B-Chat.Q4_K_M.gguf")
+# company
+llm = Llama(
+    # company
+    model_path="C:/Users/2000101497/.cache/modelscope/hub/models/Xorbits/Qwen-7B-Chat-GGUF/Qwen-7B-Chat.Q4_K_M.gguf"
+    # home
+    # model_path="C:/Users/Administrator/.cache/modelscope/hub/models/Xorbits/Qwen-7B-Chat-GGUF/Qwen-7B-Chat.Q4_K_M.gguf"
+)
 output = llm("Hello, how are you?", max_tokens=32)
 print(output["choices"][0]["text"])
